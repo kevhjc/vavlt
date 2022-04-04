d83b10fbb1150d2837470a3386f066bee83e0434
Kevin Chung <kevin.c@me.com>
2022-04-03 23:10:12 -0700

    feat: remove unused pages, add placeholder pages

---
+++ pages/brands.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+import type { NextPage } from 'next';
+
+import Container from '../components/Container';
+
+const Brands: NextPage = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Vault — Brands</title>
+      </Head>
+    </Container>
+  );
+};
+
+export default Brands;
---
+++ pages/saved.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+import type { NextPage } from 'next';
+
+import Container from '../components/Container';
+
+const Saved: NextPage = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Vault — Saved</title>
+      </Head>
+    </Container>
+  );
+};
+
+export default Saved;
