32ce1d42561c6a662611298f42973953193df31b
Kevin Chung <kevin.c@me.com>
2022-04-06 19:17:54 -0700

    refactor: import api types

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,15 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 export default async function handler(
-  req: { query: { secret: string | undefined } },
-  res: {
-    status: (arg0: number) => {
-      (): any;
-      new (): any;
-      json: { (arg0: { message: string }): any; new (): any };
-      send: { (arg0: string): any; new (): any };
-    };
-    unstable_revalidate: (arg0: string) => any;
-    json: (arg0: { revalidated: boolean }) => any;
-  }
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' });
